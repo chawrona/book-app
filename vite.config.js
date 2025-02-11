9253691d20208777c0d3ce9bb4043c2fd31a5f0b
chawrona <dawidchawrona@gmail.com>
2025-02-11 22:14:10 +0100

    Configured Vite, set up main route and main view of the app

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite';
+import vue from "@vitejs/plugin-vue";
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
     ],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000/api',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
 });
