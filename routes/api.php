b7c65c90508a313c54dfab8413b97224732e4e6b
chawrona <dawidchawrona@gmail.com>
2025-02-11 17:21:57 +0100

    Set up routes for Book and Review Controllers

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::apiResource('books',BookController::class);
+
+Route::apiResource('books.reviews',ReviewController::class)
+    ->scoped(['review' => 'book']);
