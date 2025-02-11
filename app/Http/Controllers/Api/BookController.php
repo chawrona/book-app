5f9e5e215710944f27cd4f79f9c3fe361e000ca0
chawrona <dawidchawrona@gmail.com>
2025-02-11 20:37:57 +0100

    Changes to Controller.php, bootstrap/app.php, so the middleware in Auth Controller can be seen

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -10,6 +10,11 @@ use Illuminate\Http\Request;
 class BookController extends Controller
 {
 
+    public function __construct() {
+        $this->middleware("auth:sanctum")->except(['index', 'show']);
+    }
+
+
     public function index()
     {
         return BookResource::collection(Book::with('reviews')->get());
